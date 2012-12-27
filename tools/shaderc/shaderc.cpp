9473fb1ffd0488c4f8a9affd6092f4f5aad983e7
bkaradzic <branimirkaradzic@gmail.com>
2012-12-26 22:52:10 -0800

    Fixed eol.

--- tools/shaderc/shaderc.cpp
+++ tools/shaderc/shaderc.cpp
@@ -666,6 +666,7 @@ bool compileHLSLShaderDx9(bx::CommandLine& _cmdLine, const std::string& _code, b
 		bx::write(_writer, un.regIndex);
 		bx::write(_writer, un.regCount);
 
+		BX_UNUSED(s_constantTypeName);
 		BX_TRACE("%s, %s, %d, %d, %d"
 			, un.name.c_str()
 			, s_constantTypeName[un.type]
