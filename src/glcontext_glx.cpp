5afe69f2404345d6353046eb31736ef4be0e485d
bkaradzic <branimirkaradzic@gmail.com>
2013-03-10 23:18:38 -0700

    GLX: Removed alpha from backbuffer attributes. Fix for NV issue#23.

--- src/glcontext_glx.cpp
+++ src/glcontext_glx.cpp
@@ -47,10 +47,10 @@ namespace bgfx
 			GLX_RED_SIZE, 8,
 			GLX_BLUE_SIZE, 8,
 			GLX_GREEN_SIZE, 8,
-			GLX_ALPHA_SIZE, 8,
+//			GLX_ALPHA_SIZE, 8,
 			GLX_DEPTH_SIZE, 24,
 			GLX_STENCIL_SIZE, 8,
-			None,
+			0,
 		};
 
 		// Find suitable config
