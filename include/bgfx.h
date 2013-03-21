8d63a9bdc827ce74d59cda8bb79b5a9b8c579c04
bkaradzic <branimirkaradzic@gmail.com>
2013-03-20 22:57:07 -0700

    Added VS attrib doc.

--- include/bgfx.h
+++ include/bgfx.h
@@ -242,23 +242,23 @@ namespace bgfx
 
 	struct Attrib
 	{
-		enum Enum
+		enum Enum // corresponds to vertex shader attribute:
 		{
-			Position,
-			Normal,
-			Tangent,
-			Color0,
-			Color1,
-			Indices,
-			Weight,
-			TexCoord0,
-			TexCoord1,
-			TexCoord2,
-			TexCoord3,
-			TexCoord4,
-			TexCoord5,
-			TexCoord6,
-			TexCoord7,
+			Position,  // a_position
+			Normal,    // a_normal
+			Tangent,   // a_tangent
+			Color0,    // a_color0
+			Color1,    // a_color1
+			Indices,   // a_indices
+			Weight,    // a_weight
+			TexCoord0, // a_texcoord0
+			TexCoord1, // a_texcoord1
+			TexCoord2, // a_texcoord2
+			TexCoord3, // a_texcoord3
+			TexCoord4, // a_texcoord4
+			TexCoord5, // a_texcoord5
+			TexCoord6, // a_texcoord6
+			TexCoord7, // a_texcoord7
 
 			Count
 		};
