fa9b650b375f731516c628fc6a611f699024ec34
bkaradzic <branimirkaradzic@gmail.com>
2014-03-16 16:49:59 -0700

    Added entry for asmjs.

---
+++ examples/common/entry/entry_asmjs.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2011-2014 Branimir Karadzic. All rights reserved.
+ * License: http://www.opensource.org/licenses/BSD-2-Clause
+ */
+
+#include "entry_p.h"
+
+#if BX_PLATFORM_EMSCRIPTEN
+
+#include <emscripten.h>
+
+namespace entry
+{
+	const Event* poll()
+	{
+	}
+
+	void release(const Event* /*_event*/)
+	{
+	}
+
+	void setWindowSize(uint32_t /*_width*/, uint32_t /*_height*/)
+	{
+	}
+
+	void toggleWindowFrame()
+	{
+	}
+
+	void setMouseLock(bool /*_lock*/)
+	{
+	}
+}
+
+int main(int _argc, char** _argv)
+{
+	return entry::main(_argc, _argv);
+}
+
+#endif // BX_PLATFORM_EMSCRIPTEN
