96e07d3929eb01d999a4e1c553df543b05e7b719
bkaradzic <branimirkaradzic@gmail.com>
2014-09-07 17:46:52 -0700

    Fixed OSX build.

--- src/glcontext_nsgl.h
+++ src/glcontext_nsgl.h
@@ -10,6 +10,8 @@
 
 namespace bgfx
 {
+	struct SwapChainGL;
+
 	struct GlContext
 	{
 		GlContext()
@@ -20,7 +22,12 @@ namespace bgfx
 		void create(uint32_t _width, uint32_t _height);
 		void destroy();
 		void resize(uint32_t _width, uint32_t _height, bool _vsync);
-		void swap();
+
+		SwapChainGL* createSwapChain(void* _nwh);
+		void destorySwapChain(SwapChainGL*  _swapChain);
+		void swap(SwapChainGL* _swapChain = NULL);
+		void makeCurrent(SwapChainGL* _swapChain = NULL);
+
 		void import();
 
 		bool isValid() const
--- src/glcontext_nsgl.mm
+++ src/glcontext_nsgl.mm
@@ -88,11 +88,27 @@ namespace bgfx
 		[glContext setValues:&interval forParameter:NSOpenGLCPSwapInterval];
 	}
 
-	void GlContext::swap()
+	SwapChainGL* GlContext::createSwapChain(void* /*_nwh*/)
 	{
+		BX_CHECK(false, "Shouldn't be called!");
+		return NULL;
+	}
+
+	void GlContext::destorySwapChain(SwapChainGL*  /*_swapChain*/)
+	{
+		BX_CHECK(false, "Shouldn't be called!");
+	}
+
+	void GlContext::swap(SwapChainGL* _swapChain)
+	{
+		BX_CHECK(NULL == _swapChain, "Shouldn't be called!"); BX_UNUSED(_swapChain);
 		NSOpenGLContext* glContext = (NSOpenGLContext*)m_context;
 		[glContext makeCurrentContext];
 		[glContext flushBuffer];
+	}
+
+	void GlContext::makeCurrent(SwapChainGL* /*_swapChain*/)
+	{
 	}
 
 	void GlContext::import()
