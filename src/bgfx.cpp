a0068524a9d4416c62e706c7e0374e9d3941bae1
Branimir Karadžić <branimirkaradzic@gmail.com>
2014-10-17 21:01:39 -0700

    Fixed MinGW macro collsion.

--- src/bgfx.cpp
+++ src/bgfx.cpp
@@ -1315,7 +1315,7 @@ namespace bgfx
 
 	static RendererDestroyFn s_rendererDestroyFn;
 
-	bool windowsVersionIsOrAbove(uint32_t _winver)
+	bool windowsVersionIsOrAbove(uint32_t _version)
 	{
 #if BX_PLATFORM_WINDOWS
 		OSVERSIONINFOEXA ovi;
@@ -1325,8 +1325,8 @@ namespace bgfx
 		// _WIN32_WINNT_WIN8    0x0602
 		// _WIN32_WINNT_WIN7    0x0601
 		// _WIN32_WINNT_VISTA   0x0600
-		ovi.dwMajorVersion = HIBYTE(_winver);
-		ovi.dwMinorVersion = LOBYTE(_winver);
+		ovi.dwMajorVersion = HIBYTE(_version);
+		ovi.dwMinorVersion = LOBYTE(_version);
 		DWORDLONG cond = 0;
 		VER_SET_CONDITION(cond, VER_MAJORVERSION, VER_GREATER_EQUAL);
 		VER_SET_CONDITION(cond, VER_MINORVERSION, VER_GREATER_EQUAL);
@@ -1346,6 +1346,7 @@ again:
 			{
 				RendererType::Enum first  = RendererType::Direct3D9;
 				RendererType::Enum second = RendererType::Direct3D11;
+
 				if (windowsVersionIsOrAbove(0x0603) )
 				{
 					first  = RendererType::Direct3D11 /* Direct3D12 */;
