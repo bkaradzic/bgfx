5129b8462e0fa5e98505faac1436b0613cfabd6d
Branimir Karadžić <branimirkaradzic@gmail.com>
2015-01-24 22:19:42 -0800

    Updated DX12 stub.

---
+++ src/renderer_d3d12.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2011-2015 Branimir Karadzic. All rights reserved.
+ * License: http://www.opensource.org/licenses/BSD-2-Clause
+ */
+
+#include "bgfx_p.h"
+
+namespace bgfx
+{
+	RendererContextI* rendererCreateD3D12()
+	{
+		return NULL;
+	}
+
+	void rendererDestroyD3D12()
+	{
+	}
+} // namespace bgfx
