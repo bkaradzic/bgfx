c92bff06bba56d33a2bce87858bec55f5f8148c0
Branimir Karadžić <branimirkaradzic@gmail.com>
2015-03-18 20:20:26 -0700

    Fixed GCC warnings.

--- 3rdparty/ocornut-imgui/imgui.cpp
+++ 3rdparty/ocornut-imgui/imgui.cpp
@@ -353,6 +353,8 @@
 #endif
 #ifdef __GNUC__
 #pragma GCC diagnostic ignored "-Wunused-function"          // warning: 'xxxx' defined but not used
+#pragma GCC diagnostic ignored "-Wunused-parameter"         // warning: unused parameter ‘xxxx’
+#pragma GCC diagnostic ignored "-Wtype-limits"              // warning: comparison is always true due to limited range of data type
 #endif
 
 //-------------------------------------------------------------------------
