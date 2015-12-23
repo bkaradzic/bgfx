b5f3cc34f78d750780fd3ebf9b6bfaa389417606
Branimir Karadžić <branimirkaradzic@gmail.com>
2015-12-22 21:27:03 -0800

    tinyexr: Fixed OSX warning.

--- 3rdparty/tinyexr/tinyexr.h
+++ 3rdparty/tinyexr/tinyexr.h
@@ -4377,7 +4377,7 @@ static FILE *mz_freopen(const char *pPath, const char *pMode, FILE *pStream) {
 #define MZ_FFLUSH fflush
 #define MZ_FREOPEN(f, m, s) freopen(f, m, s)
 #define MZ_DELETE_FILE remove
-#elif defined(__GNUC__) && _LARGEFILE64_SOURCE
+#elif defined(__GNUC__) && defined(_LARGEFILE64_SOURCE) && _LARGEFILE64_SOURCE
 #ifndef MINIZ_NO_TIME
 #include <utime.h>
 #endif
