2b77088a3802e17a4be4b5590586f3c58b176f82
Branimir Karadžić <branimirkaradzic@gmail.com>
2016-03-05 11:03:52 -0800

    Cleanup.

--- scripts/shader-embeded.mk
+++ scripts/shader-embeded.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2011-2015 Branimir Karadzic. All rights reserved.
+# Copyright 2011-2016 Branimir Karadzic. All rights reserved.
 # License: http://www.opensource.org/licenses/BSD-2-Clause
 #
 
