b95833f09f962b747d1bc9c6a813884cab9fab0d
Branimir Karadžić <branimirkaradzic@gmail.com>
2016-03-30 14:24:58 -0700

    Cleanup.

--- src/renderer_d3d11.cpp
+++ src/renderer_d3d11.cpp
@@ -2802,6 +2802,7 @@ BX_PRAGMA_DIAGNOSTIC_POP();
 			{
 				uint32_t cull = (_state&BGFX_STATE_CULL_MASK)>>BGFX_STATE_CULL_SHIFT;
 
+#if BX_PLATFORM_WINDOWS
 				if (m_deviceInterfaceVersion >= 3)
 				{
 					D3D11_RASTERIZER_DESC2 desc;
@@ -2825,6 +2826,7 @@ BX_PRAGMA_DIAGNOSTIC_POP();
 					DX_CHECK(device3->CreateRasterizerState2(&desc, reinterpret_cast<ID3D11RasterizerState2**>(&rs) ) );
 				}
 				else
+#endif // BX_PLATFORM_WINDOWS
 				{
 					D3D11_RASTERIZER_DESC desc;
 					desc.FillMode = _wireframe ? D3D11_FILL_WIREFRAME : D3D11_FILL_SOLID;
