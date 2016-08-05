5795fe062d4b04fbfd349655c7971e2b8388f2e5
Doruk Turak <Doruk Turak>
2016-08-06 01:47:13 +0200

    Added sdl_set_window for C99 interface.

--- include/bgfx/c99/bgfxplatform.h
+++ include/bgfx/c99/bgfxplatform.h
@@ -200,4 +200,33 @@ typedef struct bgfx_interface_vtbl
 
 typedef bgfx_interface_vtbl_t* (*PFN_BGFX_GET_INTERFACE)(uint32_t _version);
 
+bool sdl_set_window(SDL_Window* _window) {
+  SDL_SysWMinfo wmi;
+  SDL_VERSION(&wmi.version);
+  if (!SDL_GetWindowWMInfo(_window, &wmi)) {
+    return false;
+  }
+
+  bgfx_platform_data_t pd;
+#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+  pd.ndt = wmi.info.x11.display;
+  pd.nwh = (void*)(uintptr_t)wmi.info.x11.window;
+#elif BX_PLATFORM_OSX
+  pd.ndt = NULL;
+  pd.nwh = wmi.info.cocoa.window;
+#elif BX_PLATFORM_WINDOWS
+  pd.ndt = NULL;
+  pd.nwh = wmi.info.win.window;
+#elif BX_PLATFORM_STEAMLINK
+  pd.ndt = wmi.info.vivante.display;
+  pd.nwh = wmi.info.vivante.window;
+#endif  // BX_PLATFORM_
+  pd.context = NULL;
+  pd.backBuffer = NULL;
+  pd.backBufferDS = NULL;
+  bgfx_set_platform_data(&pd);
+
+  return true;
+}
+
 #endif // BGFX_PLATFORM_C99_H_HEADER_GUARD
