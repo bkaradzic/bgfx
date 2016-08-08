c25a273e29d01daef87445456c2de6ed06e0c701
Branimir Karadžić <branimirkaradzic@gmail.com>
2016-08-08 09:23:42 -0700

    Cleanup.

--- examples/common/nanovg/nanovg_bgfx.cpp
+++ examples/common/nanovg/nanovg_bgfx.cpp
@@ -715,29 +715,23 @@ namespace
 
 			int allocated = gl->tvb.size/gl->tvb.stride;
 
-			if (allocated < gl->nverts) {
+			if (allocated < gl->nverts)
+			{
 				gl->nverts = allocated;
 				BX_WARN(true, "Vertex number truncated due to transient vertex buffer overflow");
 			}
 
-
 			memcpy(gl->tvb.data, gl->verts, gl->nverts * sizeof(struct NVGvertex) );
 
-			if (gl->state == 0)
+			if (0 == gl->state)
 			{
-// 			if (alphaBlend == NVG_PREMULTIPLIED_ALPHA)
-// 			{
-// 				gl->state |= BGFX_STATE_BLEND_FUNC_SEPARATE(
-// 								  BGFX_STATE_BLEND_SRC_ALPHA, BGFX_STATE_BLEND_INV_SRC_ALPHA
-// 								, BGFX_STATE_BLEND_ONE,       BGFX_STATE_BLEND_INV_SRC_ALPHA
-// 								);
-// 			}
-// 			else
-				gl->state = BGFX_STATE_BLEND_FUNC(
-								BGFX_STATE_BLEND_SRC_ALPHA, BGFX_STATE_BLEND_INV_SRC_ALPHA
-								);
+				gl->state = BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_SRC_ALPHA, BGFX_STATE_BLEND_INV_SRC_ALPHA);
 			}
-			gl->state |= BGFX_STATE_RGB_WRITE | BGFX_STATE_ALPHA_WRITE;
+
+			gl->state |= 0
+				| BGFX_STATE_RGB_WRITE
+				| BGFX_STATE_ALPHA_WRITE
+				;
 
 			bgfx::setUniform(gl->u_viewSize, gl->view);
 
