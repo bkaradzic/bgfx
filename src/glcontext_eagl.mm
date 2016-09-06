57de9efd103eb0db8c35ba26f2a53ac2d9aa7be2
powof2 <powof2@users.noreply.github.com>
2016-09-06 20:12:34 +0800

    make glcontext_eagl.mm more like glcontext_egl.cpp
    So glcontext_eagle.mm will work with context created by other libraray/engies (like glcontext_egl.cpp)

--- src/glcontext_eagl.mm
+++ src/glcontext_eagl.mm
@@ -155,56 +155,59 @@ namespace bgfx { namespace gl
 	{
 		s_opengles = bx::dlopen("/System/Library/Frameworks/OpenGLES.framework/OpenGLES");
 		BX_CHECK(NULL != s_opengles, "OpenGLES dynamic library is not found!");
+		
+		if (NULL == g_platformData.context)
+		{
+			BX_UNUSED(_width, _height);
+			CAEAGLLayer* layer = (CAEAGLLayer*)g_platformData.nwh;
+			layer.opaque = [layer.style valueForKey:@"opaque"] == nil ? true : [[layer.style valueForKey:@"opaque"] boolValue];
 
-		BX_UNUSED(_width, _height);
-		CAEAGLLayer* layer = (CAEAGLLayer*)g_platformData.nwh;
-		layer.opaque = [layer.style valueForKey:@"opaque"] == nil ? true : [[layer.style valueForKey:@"opaque"] boolValue];
-
-		layer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys
-										: [NSNumber numberWithBool:false]
-										, kEAGLDrawablePropertyRetainedBacking
-										, kEAGLColorFormatRGBA8
-										, kEAGLDrawablePropertyColorFormat
-										, nil
-										];
-
-		EAGLContext* context = [ [EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
-		BX_CHECK(NULL != context, "Failed to create kEAGLRenderingAPIOpenGLES2 context.");
-		m_context = (void*)context;
-		[EAGLContext setCurrentContext:context];
-
-		GL_CHECK(glGenFramebuffers(1, &m_fbo) );
-		GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_fbo) );
-
-		GL_CHECK(glGenRenderbuffers(1, &m_colorRbo) );
-		GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_colorRbo) );
-
-		[context renderbufferStorage:GL_RENDERBUFFER fromDrawable:layer];
-		GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_colorRbo) );
-
-		GLint width;
-		GLint height;
-		GL_CHECK(glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &width) );
-		GL_CHECK(glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &height) );
-		BX_TRACE("Screen size: %d x %d", width, height);
-
-		GL_CHECK(glGenRenderbuffers(1, &m_depthStencilRbo) );
-		GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_depthStencilRbo) );
-		GL_CHECK(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, width, height) ); // from OES_packed_depth_stencil
-		GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_depthStencilRbo) );
-		GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_depthStencilRbo) );
-
-		BX_CHECK(GL_FRAMEBUFFER_COMPLETE ==  glCheckFramebufferStatus(GL_FRAMEBUFFER)
-			, "glCheckFramebufferStatus failed 0x%08x"
-			, glCheckFramebufferStatus(GL_FRAMEBUFFER)
-			);
-
-		makeCurrent();
-		GL_CHECK(glClearColor(0.0f, 0.0f, 0.0f, 0.0f) );
-		GL_CHECK(glClear(GL_COLOR_BUFFER_BIT) );
-		swap(NULL);
-		GL_CHECK(glClear(GL_COLOR_BUFFER_BIT) );
-		swap(NULL);
+			layer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys
+											: [NSNumber numberWithBool:false]
+											, kEAGLDrawablePropertyRetainedBacking
+											, kEAGLColorFormatRGBA8
+											, kEAGLDrawablePropertyColorFormat
+											, nil
+											];
+	
+			EAGLContext* context = [ [EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+			BX_CHECK(NULL != context, "Failed to create kEAGLRenderingAPIOpenGLES2 context.");
+			m_context = (void*)context;
+			[EAGLContext setCurrentContext:context];
+	
+			GL_CHECK(glGenFramebuffers(1, &m_fbo) );
+			GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_fbo) );
+	
+			GL_CHECK(glGenRenderbuffers(1, &m_colorRbo) );
+			GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_colorRbo) );
+	
+			[context renderbufferStorage:GL_RENDERBUFFER fromDrawable:layer];
+			GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_colorRbo) );
+	
+			GLint width;
+			GLint height;
+			GL_CHECK(glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &width) );
+			GL_CHECK(glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &height) );
+			BX_TRACE("Screen size: %d x %d", width, height);
+	
+			GL_CHECK(glGenRenderbuffers(1, &m_depthStencilRbo) );
+			GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_depthStencilRbo) );
+			GL_CHECK(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, width, height) ); // from OES_packed_depth_stencil
+			GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_depthStencilRbo) );
+			GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_depthStencilRbo) );
+	
+			BX_CHECK(GL_FRAMEBUFFER_COMPLETE ==  glCheckFramebufferStatus(GL_FRAMEBUFFER)
+				, "glCheckFramebufferStatus failed 0x%08x"
+				, glCheckFramebufferStatus(GL_FRAMEBUFFER)
+				);
+	
+			makeCurrent();
+			GL_CHECK(glClearColor(0.0f, 0.0f, 0.0f, 0.0f) );
+			GL_CHECK(glClear(GL_COLOR_BUFFER_BIT) );
+			swap(NULL);
+			GL_CHECK(glClear(GL_COLOR_BUFFER_BIT) );
+			swap(NULL);
+		}
 
 		import();
 
@@ -213,26 +216,29 @@ namespace bgfx { namespace gl
 
 	void GlContext::destroy()
 	{
-		if (0 != m_fbo)
+		if (m_context)
 		{
-			GL_CHECK(glDeleteFramebuffers(1, &m_fbo) );
-			m_fbo = 0;
+			if (0 != m_fbo)
+			{
+				GL_CHECK(glDeleteFramebuffers(1, &m_fbo) );
+				m_fbo = 0;
+			}
+	
+			if (0 != m_colorRbo)
+			{
+				GL_CHECK(glDeleteRenderbuffers(1, &m_colorRbo) );
+				m_colorRbo = 0;
+			}
+	
+			if (0 != m_depthStencilRbo)
+			{
+				GL_CHECK(glDeleteRenderbuffers(1, &m_depthStencilRbo) );
+				m_depthStencilRbo = 0;
+			}
+	
+			EAGLContext* context = (EAGLContext*)m_context;
+			[context release];
 		}
-
-		if (0 != m_colorRbo)
-		{
-			GL_CHECK(glDeleteRenderbuffers(1, &m_colorRbo) );
-			m_colorRbo = 0;
-		}
-
-		if (0 != m_depthStencilRbo)
-		{
-			GL_CHECK(glDeleteRenderbuffers(1, &m_depthStencilRbo) );
-			m_depthStencilRbo = 0;
-		}
-
-		EAGLContext* context = (EAGLContext*)m_context;
-		[context release];
 
 		bx::dlclose(s_opengles);
 	}
@@ -241,50 +247,53 @@ namespace bgfx { namespace gl
 	{
 		BX_UNUSED(_width, _height, _flags);
 		BX_TRACE("resize context");
-
-		if (0 != m_fbo)
+		
+		if (m_context)
 		{
-			GL_CHECK(glDeleteFramebuffers(1, &m_fbo) );
-			m_fbo = 0;
+			if (0 != m_fbo)
+			{
+				GL_CHECK(glDeleteFramebuffers(1, &m_fbo) );
+				m_fbo = 0;
+			}
+	
+			if (0 != m_colorRbo)
+			{
+				GL_CHECK(glDeleteRenderbuffers(1, &m_colorRbo) );
+				m_colorRbo = 0;
+			}
+	
+			if (0 != m_depthStencilRbo)
+			{
+				GL_CHECK(glDeleteRenderbuffers(1, &m_depthStencilRbo) );
+				m_depthStencilRbo = 0;
+			}
+	
+			GL_CHECK(glGenFramebuffers(1, &m_fbo) );
+			GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_fbo) );
+	
+			GL_CHECK(glGenRenderbuffers(1, &m_colorRbo) );
+			GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_colorRbo) );
+	
+			[((EAGLContext*)m_context) renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer*)g_platformData.nwh];
+			GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_colorRbo) );
+	
+			GLint width;
+			GLint height;
+			GL_CHECK(glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &width) );
+			GL_CHECK(glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &height) );
+			BX_TRACE("Screen size: %d x %d", width, height);
+	
+			GL_CHECK(glGenRenderbuffers(1, &m_depthStencilRbo) );
+			GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_depthStencilRbo) );
+			GL_CHECK(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, width, height) ); // from OES_packed_depth_stencil
+			GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_depthStencilRbo) );
+			GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_depthStencilRbo) );
+	
+			BX_CHECK(GL_FRAMEBUFFER_COMPLETE ==  glCheckFramebufferStatus(GL_FRAMEBUFFER)
+				, "glCheckFramebufferStatus failed 0x%08x"
+				, glCheckFramebufferStatus(GL_FRAMEBUFFER)
+				);
 		}
-
-		if (0 != m_colorRbo)
-		{
-			GL_CHECK(glDeleteRenderbuffers(1, &m_colorRbo) );
-			m_colorRbo = 0;
-		}
-
-		if (0 != m_depthStencilRbo)
-		{
-			GL_CHECK(glDeleteRenderbuffers(1, &m_depthStencilRbo) );
-			m_depthStencilRbo = 0;
-		}
-
-		GL_CHECK(glGenFramebuffers(1, &m_fbo) );
-		GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_fbo) );
-
-		GL_CHECK(glGenRenderbuffers(1, &m_colorRbo) );
-		GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_colorRbo) );
-
-		[((EAGLContext*)m_context) renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer*)g_platformData.nwh];
-		GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_colorRbo) );
-
-		GLint width;
-		GLint height;
-		GL_CHECK(glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &width) );
-		GL_CHECK(glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &height) );
-		BX_TRACE("Screen size: %d x %d", width, height);
-
-		GL_CHECK(glGenRenderbuffers(1, &m_depthStencilRbo) );
-		GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_depthStencilRbo) );
-		GL_CHECK(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, width, height) ); // from OES_packed_depth_stencil
-		GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_depthStencilRbo) );
-		GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_depthStencilRbo) );
-
-		BX_CHECK(GL_FRAMEBUFFER_COMPLETE ==  glCheckFramebufferStatus(GL_FRAMEBUFFER)
-			, "glCheckFramebufferStatus failed 0x%08x"
-			, glCheckFramebufferStatus(GL_FRAMEBUFFER)
-			);
 	}
 
 	uint64_t GlContext::getCaps() const
@@ -308,9 +317,12 @@ namespace bgfx { namespace gl
 
 		if (NULL == _swapChain)
 		{
-			GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_colorRbo) );
-			EAGLContext* context = (EAGLContext*)m_context;
-			[context presentRenderbuffer:GL_RENDERBUFFER];
+			if (m_context)
+			{
+				GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, m_colorRbo) );
+				EAGLContext* context = (EAGLContext*)m_context;
+				[context presentRenderbuffer:GL_RENDERBUFFER];
+			}
 		}
 		else
 		{
@@ -326,8 +338,11 @@ namespace bgfx { namespace gl
 
 			if (NULL == _swapChain)
 			{
-				[EAGLContext setCurrentContext:(EAGLContext*)m_context];
-				GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_fbo) );
+				if (m_context)
+				{
+					[EAGLContext setCurrentContext:(EAGLContext*)m_context];
+					GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_fbo) );
+				}
 			}
 			else
 			{
