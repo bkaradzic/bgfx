5905f98f0c10ff7e6abe99395deddbb021f410a0
Branimir Karadžić <branimirkaradzic@gmail.com>
2016-09-24 08:36:02 -0700

    Added missing file.

---
+++ src/renderer_gnm.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2011-2016 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
+ */
+
+#include "bgfx_p.h"
+
+#if BGFX_CONFIG_RENDERER_GNM
+#	include "../../bgfx-ext/src/renderer_gnm1.cpp"
+#else
+
+namespace bgfx { namespace gnm
+{
+	RendererContextI* rendererCreate()
+	{
+		return NULL;
+	}
+
+	void rendererDestroy()
+	{
+	}
+} /* namespace gnm */ } // namespace bgfx
+
+#endif // BGFX_CONFIG_RENDERER_GNM
