3d06ddbd767f76ed31ca8aa80a24a5a6a46820e0
Branimir Karadžić <branimirkaradzic@gmail.com>
2016-10-05 20:49:24 -0700

    Cleanup.

--- src/renderer_gnm.cpp
+++ src/renderer_gnm.cpp
@@ -8,25 +8,25 @@
 namespace bgfx
 {
 
-#define BGFX_DECALRE_EMBEDDED_SHADER(_name)            \
-			extern const uint8_t  _name ## _pssl[];    \
-			extern const uint32_t _name ## _pssl_size; \
-			const uint8_t  _name ## _pssl[] = { 0 };   \
-			const uint32_t _name ## _pssl_size = 1
+#define BGFX_DECLARE_EMBEDDED_SHADER(_name)                          \
+			extern const uint8_t  BX_CONCATENATE(_name, _pssl[]);    \
+			extern const uint32_t BX_CONCATENATE(_name, _pssl_size); \
+			const uint8_t  BX_CONCATENATE(_name, _pssl[]) = { 0 };   \
+			const uint32_t BX_CONCATENATE(_name, _pssl_size) = 1
 
-BGFX_DECALRE_EMBEDDED_SHADER(vs_debugfont);
-BGFX_DECALRE_EMBEDDED_SHADER(fs_debugfont);
-BGFX_DECALRE_EMBEDDED_SHADER(vs_clear);
-BGFX_DECALRE_EMBEDDED_SHADER(fs_clear0);
-BGFX_DECALRE_EMBEDDED_SHADER(fs_clear1);
-BGFX_DECALRE_EMBEDDED_SHADER(fs_clear2);
-BGFX_DECALRE_EMBEDDED_SHADER(fs_clear3);
-BGFX_DECALRE_EMBEDDED_SHADER(fs_clear4);
-BGFX_DECALRE_EMBEDDED_SHADER(fs_clear5);
-BGFX_DECALRE_EMBEDDED_SHADER(fs_clear6);
-BGFX_DECALRE_EMBEDDED_SHADER(fs_clear7);
+BGFX_DECLARE_EMBEDDED_SHADER(vs_debugfont);
+BGFX_DECLARE_EMBEDDED_SHADER(fs_debugfont);
+BGFX_DECLARE_EMBEDDED_SHADER(vs_clear);
+BGFX_DECLARE_EMBEDDED_SHADER(fs_clear0);
+BGFX_DECLARE_EMBEDDED_SHADER(fs_clear1);
+BGFX_DECLARE_EMBEDDED_SHADER(fs_clear2);
+BGFX_DECLARE_EMBEDDED_SHADER(fs_clear3);
+BGFX_DECLARE_EMBEDDED_SHADER(fs_clear4);
+BGFX_DECLARE_EMBEDDED_SHADER(fs_clear5);
+BGFX_DECLARE_EMBEDDED_SHADER(fs_clear6);
+BGFX_DECLARE_EMBEDDED_SHADER(fs_clear7);
 
-#undef BGFX_DECALRE_EMBEDDED_SHADER
+#undef BGFX_DECLARE_EMBEDDED_SHADER
 
 } // namespace bgfx
 
