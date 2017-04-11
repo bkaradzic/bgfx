6f8e9d2cafc269d4626d694a6a1143c73ccd65ef
Thibault Lescoat <thibault.lescoat@telecom-paristech.fr>
2017-04-11 17:33:58 +0200

    Embedded shaders utility

--- include/bgfx/embedded_shader.h
+++ include/bgfx/embedded_shader.h
@@ -104,7 +104,7 @@
 					BGFX_EMBEDDED_SHADER_ESSL (bgfx::RendererType::OpenGLES,   _name)              \
 					BGFX_EMBEDDED_SHADER_GLSL (bgfx::RendererType::OpenGL,     _name)              \
 					BGFX_EMBEDDED_SHADER_SPIRV(bgfx::RendererType::Vulkan,     _name)              \
-					{ bgfx::RendererType::Noop,  (const uint8_t*)"VSH\x4\x0\x0\x0\x0\x0\x0", 10 }, \
+					{ bgfx::RendererType::Noop,  (const uint8_t*)"VSH\x5\x0\x0\x0\x0\xf\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0", 30 }, \
 					{ bgfx::RendererType::Count, NULL, 0 }                                         \
 				}                                                                                  \
 			}
