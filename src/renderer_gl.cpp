f84711fc196aefa5b945f8972dd6fabba76d632e
Branimir Karadžić <branimirkaradzic@gmail.com>
2017-06-20 22:28:22 -0700

    Fixed GL.

--- src/renderer_gl.cpp
+++ src/renderer_gl.cpp
@@ -5685,6 +5685,14 @@ namespace bgfx { namespace gl
 							);
 					}
 
+//					if (usesTexture3D)
+					{
+						writeString(&writer
+							, "#define texture3DEXT    texture3D\n"
+							  "#define texture3DLodEXT texture3DLod\n"
+							);
+					}
+
 					if (130 <= version)
 					{
 						if (m_type == GL_FRAGMENT_SHADER)
