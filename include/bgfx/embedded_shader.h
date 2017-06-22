9215db2154c17944327df53a862104b70f1f05e1
Branimir Karadžić <branimirkaradzic@gmail.com>
2017-06-21 19:47:36 -0700

    noop: Fixed embedded dummy shader.

--- include/bgfx/embedded_shader.h
+++ include/bgfx/embedded_shader.h
@@ -102,7 +102,7 @@
 					BGFX_EMBEDDED_SHADER_ESSL (bgfx::RendererType::OpenGLES,   _name)              \
 					BGFX_EMBEDDED_SHADER_GLSL (bgfx::RendererType::OpenGL,     _name)              \
 					BGFX_EMBEDDED_SHADER_SPIRV(bgfx::RendererType::Vulkan,     _name)              \
-					{ bgfx::RendererType::Noop,  (const uint8_t*)"VSH\x4\x0\x0\x0\x0\x0\x0", 10 }, \
+					{ bgfx::RendererType::Noop,  (const uint8_t*)"VSH\x5\x0\x0\x0\x0\x0\x0", 10 }, \
 					{ bgfx::RendererType::Count, NULL, 0 }                                         \
 				}                                                                                  \
 			}
