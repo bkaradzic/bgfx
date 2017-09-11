adeb7f5870a081e0935866e982c9a276e057b59a
Branimir Karadžić <branimirkaradzic@gmail.com>
2017-09-10 20:58:45 -0700

    Cleanup.

--- examples/common/ps/particle_system.cpp
+++ examples/common/ps/particle_system.cpp
@@ -24,52 +24,6 @@ static const bgfx::EmbeddedShader s_embeddedShaders[] =
 	BGFX_EMBEDDED_SHADER_END()
 };
 
-static const bx::EaseFn s_easeFunc[] =
-{
-	bx::easeLinear,
-	bx::easeInQuad,
-	bx::easeOutQuad,
-	bx::easeInOutQuad,
-	bx::easeOutInQuad,
-	bx::easeInCubic,
-	bx::easeOutCubic,
-	bx::easeInOutCubic,
-	bx::easeOutInCubic,
-	bx::easeInQuart,
-	bx::easeOutQuart,
-	bx::easeInOutQuart,
-	bx::easeOutInQuart,
-	bx::easeInQuint,
-	bx::easeOutQuint,
-	bx::easeInOutQuint,
-	bx::easeOutInQuint,
-	bx::easeInSine,
-	bx::easeOutSine,
-	bx::easeInOutSine,
-	bx::easeOutInSine,
-	bx::easeInExpo,
-	bx::easeOutExpo,
-	bx::easeInOutExpo,
-	bx::easeOutInExpo,
-	bx::easeInCirc,
-	bx::easeOutCirc,
-	bx::easeInOutCirc,
-	bx::easeOutInCirc,
-	bx::easeInElastic,
-	bx::easeOutElastic,
-	bx::easeInOutElastic,
-	bx::easeOutInElastic,
-	bx::easeInBack,
-	bx::easeOutBack,
-	bx::easeInOutBack,
-	bx::easeOutInBack,
-	bx::easeInBounce,
-	bx::easeOutBounce,
-	bx::easeInOutBounce,
-	bx::easeOutInBounce,
-};
-BX_STATIC_ASSERT(BX_COUNTOF(s_easeFunc) == bx::Easing::Count);
-
 struct PosColorTexCoord0Vertex
 {
 	float m_x;
@@ -367,10 +321,10 @@ namespace ps
 
 		uint32_t render(const float _uv[4], const float* _mtxView, const float* _eye, uint32_t _first, uint32_t _max, ParticleSort* _outSort, PosColorTexCoord0Vertex* _outVertices)
 		{
-			bx::EaseFn easeRgba  = s_easeFunc[m_uniforms.m_easeRgba];
-			bx::EaseFn easePos   = s_easeFunc[m_uniforms.m_easePos];
-			bx::EaseFn easeBlend = s_easeFunc[m_uniforms.m_easeBlend];
-			bx::EaseFn easeScale = s_easeFunc[m_uniforms.m_easeScale];
+			bx::EaseFn easeRgba  = bx::getEaseFunc(m_uniforms.m_easeRgba);
+			bx::EaseFn easePos   = bx::getEaseFunc(m_uniforms.m_easePos);
+			bx::EaseFn easeBlend = bx::getEaseFunc(m_uniforms.m_easeBlend);
+			bx::EaseFn easeScale = bx::getEaseFunc(m_uniforms.m_easeScale);
 
 			Aabb aabb =
 			{
