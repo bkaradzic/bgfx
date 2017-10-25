f6616cfd4f217a656812a1babefb7f82f51f6a91
Branimir Karadžić <branimirkaradzic@gmail.com>
2017-10-25 16:09:54 -0700

    Fixed clang warning.

--- 3rdparty/stb/stb_rect_pack.h
+++ 3rdparty/stb/stb_rect_pack.h
@@ -1,4 +1,7 @@
 #if defined(__GNUC__) || defined(__clang__)
+#   if defined(__clang__)
+#	    pragma clang diagnostic ignored "-Wunknown-pragmas"
+#   endif // defined(__clang__)
 #	pragma GCC diagnostic ignored "-Wtype-limits"
 #	pragma GCC diagnostic ignored "-Wunused-function"
 #	pragma GCC diagnostic ignored "-Wunused-parameter"
