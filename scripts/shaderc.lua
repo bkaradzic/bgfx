8d56b09815fc5183987ae08448de07704cb1078a
Branimir Karadžić <branimirkaradzic@gmail.com>
2018-03-17 14:35:31 -0700

    glslang: Disabling warnings.

--- scripts/shaderc.lua
+++ scripts/shaderc.lua
@@ -13,30 +13,24 @@ project "glslang"
 
 	local GLSLANG = path.join(BGFX_DIR, "3rdparty/glslang")
 
-	configuration { "vs2012" }
-		defines {
-			"atoll=_atoi64",
-			"strtoll=_strtoi64",
-			"strtoull=_strtoui64",
-		}
-
 	configuration { "vs*" }
 		buildoptions {
 			"/wd4005", -- warning C4005: '_CRT_SECURE_NO_WARNINGS': macro redefinition
 			"/wd4100", -- warning C4100: 'inclusionDepth' : unreferenced formal parameter
 			"/wd4127", -- warning C4127: conditional expression is constant
+			"/wd4189", -- warning C4189: 'isFloat': local variable is initialized but not referenced
 			"/wd4244", -- warning C4244: '=': conversion from 'int' to 'char', possible loss of data
+			"/wd4310", -- warning C4310: cast truncates constant value
 			"/wd4456", -- warning C4456: declaration of 'feature' hides previous local declaration
 			"/wd4457", -- warning C4457: declaration of 'token' hides function parameter
 			"/wd4458", -- warning C4458: declaration of 'language' hides class member
 			"/wd4702", -- warning C4702: unreachable code
+			"/wd4715", -- warning C4715: 'spv::Builder::makeFpConstant': not all control paths return a value
 		}
 
 	configuration { "not vs*" }
 		buildoptions {
---			"-Wno-deprecated-register",
 			"-Wno-ignored-qualifiers",
---			"-Wno-inconsistent-missing-override",
 			"-Wno-missing-field-initializers",
 			"-Wno-reorder",
 			"-Wno-return-type",
@@ -59,6 +53,11 @@ project "glslang"
 	configuration { "linux-*" }
 		buildoptions {
 			"-Wno-unused-but-set-variable",
+		}
+
+	configuration { "mingw* or linux or osx" }
+		buildoptions {
+			"-fno-strict-aliasing", -- glslang has bugs if strict aliasing is used.
 		}
 
 	configuration {}
