fab9cb5c0b8783ca5c66ed693ded770b315dd9f1
Branimir Karadžić <branimirkaradzic@gmail.com>
2018-03-17 21:32:50 -0700

    spirv: Fixed stack corruption.

--- src/shader_spirv.h
+++ src/shader_spirv.h
@@ -601,7 +601,7 @@ namespace bgfx
 		bool hasType;
 		bool hasResult;
 
-		SpvOperand operand[10];
+		SpvOperand operand[16];
 	};
 
 	int32_t read(bx::ReaderI* _reader, SpvInstruction& _instruction, bx::Error* _err);
