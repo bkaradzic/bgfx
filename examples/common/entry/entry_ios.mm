28c1ffd28f94c24a581e04589635d2eb701e837d
Attila Kocsis <kocsisa78@gmail.com>
2018-12-09 10:03:21 +0100

    iOS metal fixes

--- examples/common/entry/entry_ios.mm
+++ examples/common/entry/entry_ios.mm
@@ -14,7 +14,7 @@
 #if __IPHONE_8_0 && !TARGET_IPHONE_SIMULATOR  // check if sdk/target supports metal
 #   import <Metal/Metal.h>
 #   import <QuartzCore/CAMetalLayer.h>
-//#   define HAS_METAL_SDK
+#   define HAS_METAL_SDK
 #endif
 
 #include <bgfx/platform.h>
