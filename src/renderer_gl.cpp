1157e78631069b1778bf06f50591ad905c950e5a
Sandy Carter <bwrsandman@gmail.com>
2019-10-25 14:56:54 +0200

    rebind instance data buffer if index buffer changes
    Fix case of drawing different indices of same vertex buffer using same
    instance data.
    
    This can happen in the case of conditionally drawing subsets of vertex
    buffer stored in different index buffers while re-using non-transient
    instance buffers.

--- src/renderer_gl.cpp
+++ src/renderer_gl.cpp
@@ -7159,6 +7159,8 @@ BX_TRACE("%d, %d, %d, %s", _array, _srgb, _mipAutogen, getName(_format) );
 
 					{
 						bool diffStreamHandles = false;
+						bool diffIndexBuffer = false;
+
 						for (uint32_t idx = 0, streamMask = draw.m_streamMask
 							; 0 != streamMask
 							; streamMask >>= 1, idx += 1
@@ -7216,6 +7218,8 @@ BX_TRACE("%d, %d, %d, %s", _array, _srgb, _mipAutogen, getName(_format) );
 							{
 								GL_CHECK(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0) );
 							}
+
+							diffIndexBuffer = true;
 						}
 
 						if (0 != currentState.m_streamMask)
@@ -7272,7 +7276,10 @@ BX_TRACE("%d, %d, %d, %s", _array, _srgb, _mipAutogen, getName(_format) );
 								}
 
 								program.bindAttributesEnd();
+							}
 
+							if (bindAttribs || diffStartVertex || diffIndexBuffer)
+							{
 								if (isValid(draw.m_instanceDataBuffer) )
 								{
 									GL_CHECK(glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffers[draw.m_instanceDataBuffer.idx].m_id) );
