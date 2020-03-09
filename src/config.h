9f37296761ca953bfb7fcb6105b3067e25cfa9de
Tobias Boogh <t@boogh.se>
2020-03-09 17:22:09 +0100

    Added X86 to platform to allow metal support on iOS Simulator

--- src/config.h
+++ src/config.h
@@ -53,6 +53,7 @@
 #	ifndef BGFX_CONFIG_RENDERER_METAL
 #		define BGFX_CONFIG_RENDERER_METAL (0           \
 					|| (BX_PLATFORM_IOS && BX_CPU_ARM) \
+					|| (BX_PLATFORM_IOS && BX_CPU_X86) \
 					|| (BX_PLATFORM_OSX >= 101100)     \
 					? 1 : 0)
 #	endif // BGFX_CONFIG_RENDERER_METAL
