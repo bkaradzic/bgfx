fa826a4935a84fdb33fc2ea53d64d14390184914
Бранимир Караџић <branimirkaradzic@gmail.com>
2020-09-16 09:11:15 -0700

    Fixed webgl build.

--- src/glcontext_html5.cpp
+++ src/glcontext_html5.cpp
@@ -183,21 +183,21 @@ namespace bgfx { namespace gl
 	{
 		BX_TRACE("Import:");
 
-#	define GL_EXTENSION(_optional, _proto, _func, _import)                                             \
-	{                                                                                                  \
-		if (NULL == _func)                                                                             \
-		{                                                                                              \
-			_func = (_proto)emscripten_webgl1_get_proc_address(#_import);                              \
-			if (!_func && webGLVersion >= 2)                                                           \
-			{                                                                                          \
-				_func = reinterpret_cast<ProtoT><_proto>(emscripten_webgl2_get_proc_address(#_import); \
-			}                                                                                          \
-			BX_TRACE("\t%p " #_func " (" #_import ")", _func);                                         \
-			BGFX_FATAL(_optional || NULL != _func, Fatal::UnableToInitialize                           \
-				, "Failed to create WebGL/OpenGLES context. GetProcAddress(\"%s\")"                    \
-				, #_import                                                                             \
-				);                                                                                     \
-		}                                                                                              \
+#	define GL_EXTENSION(_optional, _proto, _func, _import)                                     \
+	{                                                                                          \
+		if (NULL == _func)                                                                     \
+		{                                                                                      \
+			_func = (_proto)emscripten_webgl1_get_proc_address(#_import);                      \
+			if (!_func && webGLVersion >= 2)                                                   \
+			{                                                                                  \
+				_func = reinterpret_cast<_proto>(emscripten_webgl2_get_proc_address(#_import); \
+			}                                                                                  \
+			BX_TRACE("\t%p " #_func " (" #_import ")", _func);                                 \
+			BGFX_FATAL(_optional || NULL != _func, Fatal::UnableToInitialize                   \
+				, "Failed to create WebGL/OpenGLES context. GetProcAddress(\"%s\")"            \
+				, #_import                                                                     \
+				);                                                                             \
+		}                                                                                      \
 	}
 
 #	include "glimports.h"
