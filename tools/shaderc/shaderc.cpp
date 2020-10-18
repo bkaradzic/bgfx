72b04e391391d3999e716034925edf6d7fcfcc5b
pheonix <dev@creatingtrouble.com>
2020-10-17 20:02:51 -0700

    Shaderc SPIR-V profile additions (#2284)
    * [shaderc] Add SPIR-V profiles to support Vulkan 1.1 and Vulkan 1.2.
    
    * * Add support for Vulkan 1.1 with SPIR-V 1.4.
    * Update "spirv" values to correlate with shaderc spirv profile name mappings.
    * Expand on spirv profile naming and encoding documentation.
    * Address PR notes

--- tools/shaderc/shaderc.cpp
+++ tools/shaderc/shaderc.cpp
@@ -850,6 +850,15 @@ namespace bgfx
 	// 4.2    420               11.0     vhdgf+c  5.0
 	// 4.3    430      vhdgf+c
 	// 4.4    440
+	//
+	// SPIR-V profile naming convention:
+	//  spirv<SPIR-V version>-<Vulkan version>
+	//
+	// SPIR-V version | Vulkan version | shaderc encoding
+	//       1.0      |       1.0      |      1010
+	//       1.3      |       1.1      |      1311
+	//       1.4      |       1.1      |      1411
+	//       1.5      |       1.2      |      1512
 
 	void help(const char* _error = NULL)
 	{
@@ -894,7 +903,11 @@ namespace bgfx
 				"           s_5\n"
 				"           metal\n"
 				"           pssl\n"
-				"           spirv\n"
+				"           spirv              Alias for spirv10-10. \n"
+				"           spirv10-10\n"
+				"           spirv13-11\n"
+				"           spirv14-11\n"
+				"           spirv15-12\n"
 			  "      --preprocess              Preprocess only.\n"
 			  "      --define <defines>        Add defines to preprocessor (semicolon separated).\n"
 			  "      --raw                     Do not process shader. No preprocessor, and no glsl-optimizer (GLSL only).\n"
@@ -960,9 +973,22 @@ namespace bgfx
 			{
 				pssl = 1;
 			}
-			else if (0 == bx::strCmp(profile, "spirv") )
+			else if (0 == bx::strCmp(profile, "spirv13-11") )
 			{
-				spirv = 1;
+				spirv = 1311;
+			}
+			else if (0 == bx::strCmp(profile, "spirv14-11") )
+			{
+				spirv = 1411;
+			}
+			else if (0 == bx::strCmp(profile, "spirv15-12") )
+			{
+				spirv = 1512;
+			}
+			else if (0 == bx::strCmp(profile, "spirv10-10")
+			      || 0 == bx::strCmp(profile, "spirv") )
+			{
+				spirv = 1010;
 			}
 			else
 			{
@@ -1512,7 +1538,7 @@ namespace bgfx
 							}
 							else if (0 != spirv)
 							{
-								compiled = compileSPIRVShader(_options, 0, code, _writer);
+								compiled = compileSPIRVShader(_options, spirv, code, _writer);
 							}
 							else if (0 != pssl)
 							{
@@ -2310,7 +2336,7 @@ namespace bgfx
 							}
 							else if (0 != spirv)
 							{
-								compiled = compileSPIRVShader(_options, 0, code, _writer);
+								compiled = compileSPIRVShader(_options, spirv, code, _writer);
 							}
 							else if (0 != pssl)
 							{
