2fee82dc567e65e8c15973dbfc7a6490e4e1825b
Christophe Dehais <christophe.dehais@gmail.com>
2020-10-26 18:42:55 +0100

    Add missing preprocessor macro for ESSL (#2298)
    Co-authored-by: Christophe Dehais <christophe.dehais@fittingbox.com>

--- tools/shaderc/shaderc.cpp
+++ tools/shaderc/shaderc.cpp
@@ -1106,6 +1106,7 @@ namespace bgfx
 				preprocessor.setDefine(hlslDefine);
 			}
 			else if (0 != glsl)
+			     || (0 != essl)
 			{
 				preprocessor.setDefine(glslDefine);
 			}
@@ -1127,6 +1128,7 @@ namespace bgfx
 				preprocessor.setDefine(hlslDefine);
 			}
 			else if (0 != glsl)
+			     || (0 != essl)
 			{
 				preprocessor.setDefine(glslDefine);
 			}
