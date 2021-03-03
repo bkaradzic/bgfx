3dd78ef893f1803b84198369ee51db8c0f96b1f5
Бранимир Караџић <branimirkaradzic@gmail.com>
2021-03-03 08:27:57 -0800

    Cleanup.

--- src/renderer_vk.cpp
+++ src/renderer_vk.cpp
@@ -656,7 +656,7 @@ VK_IMPORT_DEVICE
 			;
 	}
 
-	void dumpExtensions(VkPhysicalDevice _physicalDevice = VK_NULL_HANDLE)
+	void dumpExtensions(VkPhysicalDevice _physicalDevice)
 	{
 		{ // Global extensions.
 			uint32_t numExtensionProperties;
@@ -691,7 +691,7 @@ VK_IMPORT_DEVICE
 					BX_TRACE("\tv%-3d %s%s"
 						, extensionProperties[extension].specVersion
 						, extensionProperties[extension].extensionName
-						, supported ? " (supported)" : "", extensionProperties[extension].extensionName // FIXME: maybe include here the layer name?
+						, supported ? " (supported)" : "", extensionProperties[extension].extensionName
 						);
 
 					BX_UNUSED(supported);
@@ -1428,7 +1428,7 @@ VK_IMPORT
 			}
 
 			{
-				dumpExtensions();
+				dumpExtensions(VK_NULL_HANDLE);
 
 				uint32_t numEnabledLayers = 0;
 
