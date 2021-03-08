dc3a92ef994a024c143fd3724d9cd5fbdf4a9044
Aleš Mlakar <jazzbre@gmail.com>
2021-03-08 16:48:17 +0100

    Vulkan shader compiler uniform size fix (>255). (#2421)
    Co-authored-by: Aleš Mlakar <ales.ambits@gmail.com>

--- tools/shaderc/shaderc_spirv.cpp
+++ tools/shaderc/shaderc_spirv.cpp
@@ -957,10 +957,10 @@ namespace bgfx { namespace spirv
 						Uniform un;
 						un.name = program->getUniformName(ii);
 
-						un.num = uint8_t(program->getUniformArraySize(ii) );
+						un.num = 0;
 						const uint32_t offset = program->getUniformBufferOffset(ii);
 						un.regIndex = uint16_t(offset);
-						un.regCount = un.num;
+						un.regCount = uint16_t(program->getUniformArraySize(ii));
 
 						switch (program->getUniformType(ii) )
 						{
