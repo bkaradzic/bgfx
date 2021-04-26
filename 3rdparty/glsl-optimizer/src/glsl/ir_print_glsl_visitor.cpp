29aa15725d6107a4436af073c0f2aada2358b2a5
Branimir Karadžić <branimirkaradzic@gmail.com>
2021-04-25 18:07:13 -0700

    glsl-optimizer: Fixed output of texture sampling with texture array lod.

--- 3rdparty/glsl-optimizer/src/glsl/ir_print_glsl_visitor.cpp
+++ 3rdparty/glsl-optimizer/src/glsl/ir_print_glsl_visitor.cpp
@@ -899,7 +899,13 @@ void ir_print_glsl_visitor::visit(ir_texture *ir)
 	}
 
 	if (is_array && state->EXT_texture_array_enable)
+	{
+		if(state->language_version>=130)
+		{
+			buffer.asprintf_append ("%s", tex_sampler_dim_name[sampler_dim]);
+		}
 		buffer.asprintf_append ("Array");
+	}
 	if ((ir->op == ir_tex || ir->op == ir_txl) && is_proj)
 		buffer.asprintf_append ("Proj");
 	if (ir->op == ir_txl)
