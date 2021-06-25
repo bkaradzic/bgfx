9df96732ea6a931a5220fc80b48c8482a5103e0e
bbi-timstump <tim.stump@blackbirdinteractive.com>
2021-06-25 16:24:48 -0700

    - add native entry file (just a copy of the noop one with the proper NX entry point) - add check for no command line arguments

---
+++ examples/common/entry/entry_nx.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2011-2021 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
+ */
+
+#include "entry_p.h"
+
+#if ENTRY_CONFIG_USE_NATIVE && BX_PLATFORM_NX
+
+namespace entry
+{
+	const Event* poll()
+	{
+		return NULL;
+	}
+
+	const Event* poll(WindowHandle _handle)
+	{
+		BX_UNUSED(_handle);
+		return NULL;
+	}
+
+	void release(const Event* _event)
+	{
+		BX_UNUSED(_event);
+	}
+
+	WindowHandle createWindow(int32_t _x, int32_t _y, uint32_t _width, uint32_t _height, uint32_t _flags, const char* _title)
+	{
+		BX_UNUSED(_x, _y, _width, _height, _flags, _title);
+		WindowHandle handle = { UINT16_MAX };
+		return handle;
+	}
+
+	void destroyWindow(WindowHandle _handle)
+	{
+		BX_UNUSED(_handle);
+	}
+
+	void setWindowPos(WindowHandle _handle, int32_t _x, int32_t _y)
+	{
+		BX_UNUSED(_handle, _x, _y);
+	}
+
+	void setWindowSize(WindowHandle _handle, uint32_t _width, uint32_t _height)
+	{
+		BX_UNUSED(_handle, _width, _height);
+	}
+
+	void setWindowTitle(WindowHandle _handle, const char* _title)
+	{
+		BX_UNUSED(_handle, _title);
+	}
+
+	void setWindowFlags(WindowHandle _handle, uint32_t _flags, bool _enabled)
+	{
+		BX_UNUSED(_handle, _flags, _enabled);
+	}
+
+	void toggleFullscreen(WindowHandle _handle)
+	{
+		BX_UNUSED(_handle);
+	}
+
+	void setMouseLock(WindowHandle _handle, bool _lock)
+	{
+		BX_UNUSED(_handle, _lock);
+	}
+
+} // namespace entry
+
+extern "C" void nnMain(int _argc, char* _argv[])
+{
+	int result = entry::main(_argc, _argv);
+
+	if (result != bx::kExitSuccess)
+	{
+		// report error?
+	}
+}
+
+#endif // ENTRY_CONFIG_USE_NOOP
