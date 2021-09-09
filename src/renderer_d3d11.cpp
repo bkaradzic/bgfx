bbc2b76948a5161d205751da16e912cfca0afcab
James Fulop <jameslynnfulop@gmail.com>
2021-09-09 07:20:01 -0700

    Dxgi tearing support (#2601)
    * present tearing support
    
    * use existing syncInterval variable

--- src/renderer_d3d11.cpp
+++ src/renderer_d3d11.cpp
@@ -2287,7 +2287,12 @@ namespace bgfx { namespace d3d11
 					if (NULL != m_swapChain
 					&&  m_needPresent)
 					{
-						hr = m_swapChain->Present(syncInterval, 0);
+						uint32_t presentFlags = 0;
+						if (!syncInterval && (m_dxgi.tearingSupported()))
+						{
+							presentFlags |= DXGI_PRESENT_ALLOW_TEARING;
+						}
+						hr = m_swapChain->Present(syncInterval, presentFlags);
 
 						m_needPresent = false;
 					}
