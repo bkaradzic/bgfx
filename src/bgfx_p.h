89b6c25c4a67644f41a7d82a50267fb59f37198c
Gaetan de Villele <gdevillele@gmail.com>
2021-09-28 04:07:32 +0200

    fix usage of fatal() function when Clang analyzer is used (#2617)

--- src/bgfx_p.h
+++ src/bgfx_p.h
@@ -67,7 +67,7 @@
 namespace bgfx
 {
 #if BX_COMPILER_CLANG_ANALYZER
-	void __attribute__( (analyzer_noreturn) ) fatal(Fatal::Enum _code, const char* _format, ...);
+	void __attribute__( (analyzer_noreturn) ) fatal(const char* _filePath, uint16_t _line, Fatal::Enum _code, const char* _format, ...);
 #else
 	void fatal(const char* _filePath, uint16_t _line, Fatal::Enum _code, const char* _format, ...);
 #endif // BX_COMPILER_CLANG_ANALYZER
