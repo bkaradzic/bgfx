76e78b8e7511a10a3823d9b55b953d26b42a3df2
Richard Schubert <rs@directx9.de>
2021-10-15 08:54:33 +0200

    Remove redundant calls

--- examples/46-fsr/app.cpp
+++ examples/46-fsr/app.cpp
@@ -537,8 +537,6 @@ namespace
 					bgfx::setViewRect(view, 0, 0, (uint16_t)m_state.m_width, (uint16_t)m_state.m_height);
 					bgfx::setViewClear(view, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x00000000, 1.0f, 0);
 					bgfx::setViewFrameBuffer(view, m_state.m_frameBuffer);
-					bgfx::setState(0 | BGFX_STATE_WRITE_RGB | BGFX_STATE_WRITE_A | BGFX_STATE_WRITE_Z | BGFX_STATE_DEPTH_TEST_LESS);
-					bgfx::setTexture(0, m_state.s_color, BGFX_INVALID_HANDLE);
 					bgfx::touch(view);
 
 					++view;
