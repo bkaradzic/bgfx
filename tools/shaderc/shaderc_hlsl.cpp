b24859fa3773a20e0cbf1526e373273c4a0052cb
Christophe Dehais <christophe.dehais@gmail.com>
2021-10-26 18:52:52 +0200

    Strip flags from UniformType before calling getUniformTypeName()

--- tools/shaderc/shaderc_hlsl.cpp
+++ tools/shaderc/shaderc_hlsl.cpp
@@ -753,7 +753,7 @@ namespace bgfx { namespace hlsl
 
 				BX_TRACE("%s, %s, %d, %d, %d"
 					, un.name.c_str()
-					, getUniformTypeName(un.type)
+					, getUniformTypeName(UniformType::Enum(un.type & ~kUniformMask))
 					, un.num
 					, un.regIndex
 					, un.regCount
