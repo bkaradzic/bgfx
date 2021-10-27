53772163f4803406df2db2bf5567073e9c9ed108
Бранимир Караџић <branimirkaradzic@gmail.com>
2021-10-26 18:20:35 -0700

    Added cmdRemove.

--- examples/common/entry/cmd.h
+++ examples/common/entry/cmd.h
@@ -19,6 +19,9 @@ void cmdShutdown();
 void cmdAdd(const char* _name, ConsoleFn _fn, void* _userData = NULL);
 
 ///
+void cmdRemove(const char* _name);
+
+///
 void cmdExec(const char* _format, ...);
 
 #endif // CMD_H_HEADER_GUARD
