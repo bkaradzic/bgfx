2e9ea79f27f42b1ea49e66ce7ba0a5c1ab75ea81
Бранимир Караџић <branimirkaradzic@gmail.com>
2022-01-28 19:57:47 -0800

    Updated Vulkan headers.

--- 3rdparty/khronos/vulkan-local/vulkan_android.h
+++ 3rdparty/khronos/vulkan-local/vulkan_android.h
@@ -2,7 +2,7 @@
 #define VULKAN_ANDROID_H_ 1
 
 /*
-** Copyright 2015-2021 The Khronos Group Inc.
+** Copyright 2015-2022 The Khronos Group Inc.
 **
 ** SPDX-License-Identifier: Apache-2.0
 */
@@ -95,7 +95,7 @@ typedef struct VkAndroidHardwareBufferFormatProperties2ANDROID {
     void*                            pNext;
     VkFormat                         format;
     uint64_t                         externalFormat;
-    VkFormatFeatureFlags2KHR         formatFeatures;
+    VkFormatFeatureFlags2            formatFeatures;
     VkComponentMapping               samplerYcbcrConversionComponents;
     VkSamplerYcbcrModelConversion    suggestedYcbcrModel;
     VkSamplerYcbcrRange              suggestedYcbcrRange;
