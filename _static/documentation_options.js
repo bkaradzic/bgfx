fc561e3e58d4d91216ec8f489b25e31e7f92edc6
Бранимир Караџић <branimirkaradzic@gmail.com>
2022-09-17 18:23:36 -0700

    Updated docs.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.115.8277',
-    LANGUAGE: 'None',
+    VERSION: '1.116.8293',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
