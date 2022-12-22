e3d26b75026bc8e15cddc4ec2174ae938c0d0a2c
DannyWayve <103435421+DannyWayve@users.noreply.github.com>
2022-12-22 15:06:23 +0000

    Update src/renderer_vk.cpp

--- src/renderer_vk.cpp
+++ src/renderer_vk.cpp
@@ -1979,7 +1979,7 @@ VK_IMPORT_DEVICE
 			}
 
 			{
-				const uint32_t size = 256;
+				const uint32_t size = 256; // WAYVE: Increasing size, our vertex/frag constant buffers are larger than an average of 128 (by a fair amount..)
 				const uint32_t count = BGFX_CONFIG_MAX_DRAW_CALLS;
 				for (uint32_t ii = 0; ii < m_numFramesInFlight; ++ii)
 				{
