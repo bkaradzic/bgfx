954c18b41743e706c28cf1cdfb8eb4ccc215c6c8
Бранимир Караџић <branimirkaradzic@gmail.com>
2023-03-16 20:29:57 -0700

    Updated version number.

--- src/version.h
+++ src/version.h
@@ -9,5 +9,5 @@
  *
  */
 
-#define BGFX_REV_NUMBER 8427
-#define BGFX_REV_SHA1   "dcf926f624b9642769f8486e789ec26c74e34f4b"
+#define BGFX_REV_NUMBER 8444
+#define BGFX_REV_SHA1   "b9d0d5474f110d29e251b9ea99faf5e857fc2a3b"
