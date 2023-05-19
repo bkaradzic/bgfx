b07035ae4274acdd4df630de4215b7762bd2bd60
Бранимир Караџић <branimirkaradzic@gmail.com>
2023-05-18 23:06:00 -0700

    Updated docs.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.118.8367',
+    VERSION: '1.118.8481',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
