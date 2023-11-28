2b136890a2824c0bd07a45911b1dd0889a882c72
Бранимир Караџић <branimirkaradzic@gmail.com>
2023-11-27 22:27:36 -0800

    Updated cgltf.

--- 3rdparty/cgltf/cgltf_write.h
+++ 3rdparty/cgltf/cgltf_write.h
@@ -178,8 +178,8 @@ typedef struct {
 		cgltf_write_line(context, "}"); }
 
 #ifndef CGLTF_CONSTS
-static const cgltf_size GlbHeaderSize = 12;
-static const cgltf_size GlbChunkHeaderSize = 8;
+#define GlbHeaderSize 12
+#define GlbChunkHeaderSize 8
 static const uint32_t GlbVersion = 2;
 static const uint32_t GlbMagic = 0x46546C67;
 static const uint32_t GlbMagicJsonChunk = 0x4E4F534A;
