6a8c04ea4fb30319bc0d3c308fad7658933cc550
Бранимир Караџић <branimirkaradzic@gmail.com>
2026-03-21 14:57:09 -0700

    Makefile: Detect if sh is present.

--- scripts/tools.mk
+++ scripts/tools.mk
@@ -8,20 +8,24 @@ SILENT?=@
 THISDIR:=$(dir $(lastword $(MAKEFILE_LIST)))
 
 UNAME:=$(shell uname)
-ifeq ($(UNAME),$(filter Linux Darwin MINGW%,$(UNAME)))
-CMD_MKDIR=mkdir -p "$(1)"
-CMD_RMDIR=rm -r "$(1)"
+ifeq ($(UNAME),$(filter Linux Darwin,$(UNAME)))
 ifeq ($(UNAME),$(filter Darwin,$(UNAME)))
 OS=darwin
-else ifeq ($(UNAME),$(filter MINGW%,$(UNAME)))
-OS=windows
 else
 OS=linux
 endif
+else ifeq ($(UNAME),$(filter MINGW%,$(UNAME)))
+OS=windows
+else
+OS=windows
+endif
+
+ifneq ($(findstring sh,$(notdir $(SHELL))),)
+CMD_MKDIR=mkdir -p "$(1)"
+CMD_RMDIR=rm -rf "$(1)"
 else
 CMD_MKDIR=cmd /C "if not exist "$(subst /,\,$(1))" mkdir "$(subst /,\,$(1))""
 CMD_RMDIR=cmd /C "if exist "$(subst /,\,$(1))" rmdir /S /Q "$(subst /,\,$(1))""
-OS=windows
 endif
 
 SHADERC:="$(THISDIR)../tools/bin/$(OS)/shaderc"
