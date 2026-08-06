c0b161ef61ea4d11a233f0e5ff1e0392a0392722
Branimir Karadžić <branimirkaradzic@gmail.com>
2026-08-05 17:01:17 -0700

    GL: Fixed render target MSAA on GLES 3.0 / WebGL 2. Fixes #3870. (#3873)

--- src/renderer_gl.cpp
+++ src/renderer_gl.cpp
@@ -3135,7 +3135,8 @@ namespace bgfx { namespace gl
 					GL_CHECK(glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &m_maxAnisotropyDefault) );
 				}
 
-				if (s_extension[Extension::ARB_texture_multisample].m_supported
+				if (m_gles3
+				||  s_extension[Extension::ARB_texture_multisample].m_supported
 				||  s_extension[Extension::ANGLE_framebuffer_multisample].m_supported
 				||  s_extension[Extension::EXT_multisampled_render_to_texture].m_supported)
 				{
@@ -7476,7 +7477,16 @@ namespace bgfx { namespace gl
 						GL_CHECK(glBindFramebuffer(GL_DRAW_FRAMEBUFFER, m_fbo[1]) );
 
 						GL_CHECK(glReadBuffer(GL_COLOR_ATTACHMENT0 + colorIdx) );
-						GL_CHECK(glDrawBuffer(GL_COLOR_ATTACHMENT0 + colorIdx) );
+
+						if (BX_ENABLED(BGFX_CONFIG_RENDERER_OPENGL) )
+						{
+							GL_CHECK(glDrawBuffer(GL_COLOR_ATTACHMENT0 + colorIdx) );
+						}
+						else
+						{
+							const GLenum drawBuffer = GL_COLOR_ATTACHMENT0 + colorIdx;
+							GL_CHECK(glDrawBuffers(1, &drawBuffer) );
+						}
 
 						colorIdx++;
 
@@ -7489,7 +7499,7 @@ namespace bgfx { namespace gl
 							, m_width
 							, m_height
 							, GL_COLOR_BUFFER_BIT
-							, GL_LINEAR
+							, GL_NEAREST
 							) );
 					}
 					else if (!writeOnly)
