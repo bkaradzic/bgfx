c772ecc3f559db0bdeeb3fbbe98fbaca1313dee2
Lcbx <Lcbx@users.noreply.github.com>
2026-08-21 20:27:11 +0200

    add python bindings generator  (#3915)
    * add python binding generator
    
    * add idl generated python binding

--- scripts/genie.lua
+++ scripts/genie.lua
@@ -87,6 +87,9 @@ newaction {
 			local c3gen = require "bindings-c3"
 			c3gen.write(c3gen.gen(), "../bindings/c3/bgfx.c3")
 
+			local pygen = require "bindings-py"
+			pygen.write(pygen.gen(), "../bindings/py/bgfx.py")
+
 			local docsgen = require "docs-rst"
 			docsgen.write(docsgen.gen(), "../docs/bgfx.rst")
 		end
